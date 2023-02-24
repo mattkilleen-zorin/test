6a0e5db824a067b891a99efa0239396a81bf9ec6
mkilleen-mn <mkilleen@modeln.com>
2023-02-24 15:22:47 -0500

    Version using ord() range.

--- pwd_gen.py
+++ pwd_gen.py
@@ -1,18 +1,11 @@
-#!/bin/python3
-#
-#     pwd-gen.py
-#
-#     A bit like the Google Chrome password generator.
-#
-
 import random
 
 sPassword = ""
 nSwitch = 0
 
-randChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+alphaList = [chr(chNum) for chNum in list(range(ord('a'),ord('z')+1))]
+numList = [chr(chNum) for chNum in list(range(ord('0'),ord('9')+1))]
+randChars = alphaList + numList
 
 for n in range(0,20):
     if n == 6 or n == 13:
@@ -23,5 +16,5 @@ for n in range(0,20):
             sPassword += random.choice(randChars)
         else:
             sPassword += random.choice(randChars).upper()
-    
+   
 print(sPassword)
